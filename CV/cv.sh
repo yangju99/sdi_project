b26111fa553525b8b8d5c8a90957efa2916a50f7
yangju99 <didwndud3299@naver.com>
2025-06-15 15:36:46 +0900

    second commit

--- CV/cv.sh
+++ CV/cv.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+export TURTLEBOT3_MODEL=waffle
+source ~/.bashrc
 
 # shell 1: Gazebo 시뮬레이터 실행
-gnome-terminal -- bash -c "export FORCE_BASHRC=1; export TURTLEBOT3_MODEL=burger; source ~/.bashrc; ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py; exec bash"
+ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py &
+PID1=$!
 sleep 5  # Gazebo가 충분히 로딩될 시간
 
 # shell 2: Navigation2 실행
-gnome-terminal -- bash -c "export FORCE_BASHRC=1; export TURTLEBOT3_MODEL=burger; source ~/.bashrc; ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$HOME/map.yaml; exec bash"
+ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$HOME/map.yaml &
+PID2=$!
 sleep 5  # nav2 bringup 시간 확보
 
 # shell 3: 자동 목표 이동 Python 스크립트 실행
-gnome-terminal -- bash -c "export FORCE_BASHRC=1; export TURTLEBOT3_MODEL=burger; source ~/.bashrc; python3 \$HOME/single_goal_nav.py; exec bash"
+python3 $HOME/single_goal_nav.py &
+PID3=$!
 
 
 
