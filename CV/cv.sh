ea64e07f91634c6900e5d633394188c6f8151328
yangju99 <didwndud3299@naver.com>
2025-06-15 16:18:21 +0900

    hihi

--- CV/cv.sh
+++ CV/cv.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-# GUI 환경 전달
-export DISPLAY=:0
-export XAUTHORITY=/home/webeng/.Xauthority
-export TURTLEBOT3_MODEL=waffle
-export FORCE_BASHRC=1
-source /home/webeng/.bashrc
+# shell 1: Gazebo 시뮬레이터 실행
+gnome-terminal -- bash -c "export FORCE_BASHRC=1; export TURTLEBOT3_MODEL=burger; source ~/.bashrc; ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py; exec bash"
+sleep 5  # Gazebo가 충분히 로딩될 시간
 
-# Gazebo
-gnome-terminal -- bash -c "ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py; exec bash"
-sleep 5
+# shell 2: Navigation2 실행
+gnome-terminal -- bash -c "export FORCE_BASHRC=1; export TURTLEBOT3_MODEL=burger; source ~/.bashrc; ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$HOME/map.yaml; exec bash"
+sleep 5  # nav2 bringup 시간 확보
 
-# Navigation2
-gnome-terminal -- bash -c "ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:/home/webeng/map.yaml; exec bash"
-sleep 5
-
-# 목표 이동 스크립트
-gnome-terminal -- bash -c "python3 /home/webeng/single_goal_nav.py; exec bash"
+# shell 3: 자동 목표 이동 Python 스크립트 실행
+gnome-terminal -- bash -c "export FORCE_BASHRC=1; export TURTLEBOT3_MODEL=burger; source ~/.bashrc; python3 \$HOME/single_goal_nav.py; exec bash"
 
 
 
